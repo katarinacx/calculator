6c1f9d24d7bf0e7d1445b677645a01fbae2d37d8
Tarin Chance <katarinacx@hotmail.com>
2020-04-10 14:15:07 +0100

    calculator 1.2 build, a bit more advanced with more buttons

--- js/main.js
+++ js/main.js
@@ -1,165 +1,77 @@
+//variables with i_ = int
+//variables with s_ = string
+
 //Need to import the $ function from jquery so I can use it in this file. This is due to webpack compiler and ES6.
 import $ from "jquery";
 
-var number_one;
-var number_two;
+//Declaring these variables in the global scope so they can be accessed by any function (e.g. we're gonna need these when the user presses the equals operator)
+var s_operation;
+var i_number_one;
+var i_number_two;
 
-// function which gets the two numbers from the input boxes and stores them in variables
-function getNumbers() {
-    //     Store the value of number one in a variable
-    number_one = parseInt($("#number_one").val());
-    //     Store the value of number two in a variable
-    number_two = parseInt($("#number_two").val());
+//function to get each number whenever the user clicks a number button
+window.appendNumber = function(i_number) {
+    //This is putting the value of the HTML element into its own variable.
+    var s_current_display = $('#sum').val();
 
-    // Need to check whether there's a valid input in the input boxes, if number one is not a number then we will add a class of error and change the variable to false, this will then skip the else statement afterwards. The second if statement is the same, if it's true for example it will remove any error classes and return isValid, but because one of the numbers hasn't been inputted the isValid will be set to false and will not run the code.
-    var isValid = true;
-    if (Number.isNaN(number_one)) {
-        $('#number_one').addClass('error');
-        isValid = false;
-    } else {
-        $('#number_one').removeClass('error');
+    //Strings take precedence over ints. So, even though we're passing through an int parameter, it will change to a string because the current_display is a string
+    //We are appending the number into the current_display (Don't forget these are now both strings)
+    var s_new_display = s_current_display + i_number;
+
+    //Putting the new_display variable into the HTML element so it shows in the input field (as a value).
+    $('#sum').val(s_new_display);
+};
+
+window.setNumberFromDisplay = function() {
+    //If there's no operator stored then set the value of the sum HTML element to number_one
+    if (!s_operation) {
+        i_number_one = parseInt($('#sum').val());
     }
-
-    if (Number.isNaN(number_two)) {
-        $('#number_two').addClass('error');
-        isValid = false;
-    } else {
-        $('#number_two').removeClass('error');
+    //If there is then set the value of the sum HTML element to number_two
+    else {
+        i_number_two = parseInt($('#sum').val());
     }
+};
 
-    return isValid;
-}
+window.setOperation = function(s_operator) {
 
-// function which outputs the calculated number on the screen
-//      Accept parameter of the number and operator to display on screen
-function outputResult(result, operator) {
-    //     Put the number in the answers input box
-    getNumbers();
-    // Gets the ID of the input box sum and appends this into the input box. 
-    $('#sum').val(number_one + operator + number_two + '=' + result);
-}
+    //Store the current number into a variable that the user has inputted 
+    //Using the function above
+    window.setNumberFromDisplay();
 
+    //Store operation into a variable so we can use it later when the user presses the equals button
+    //Getting the param from the HTML
+    s_operation = s_operator;
 
-// function which clears the two input boxes
-function clearNumbers() {
-    //     Set the number one input to empty
-    number_one = $("#number_one").val('');
-    //     Set the number two input to empty
-    number_two = $("#number_two").val('');
-}
+    //Clear the input field ready for the user to tell us the second number
+    $('#sum').val('');
+};
 
-//function which is called when the user presses any of the equation buttons
-window.numberEquation = function(operation) {
-    //Defining the two variables here
+window.result = function() {
+    //Store the second number into a variable, getting the value from the element
+    //Using the function above
+    window.setNumberFromDisplay();
+    //Declare a variable to hold the outcome of the equation
     var result;
-    var operator;
 
-    //If no numbers have been gotten (not true) then we will return false and stop doing anymore.
-    if (!getNumbers()) {
-        return false;
-    }
-
-    //This is a shorter and better way of handling than doing a function for each operation
-    //Im passing operation as a param in the function and using it for this switch (The operation param is determined on what we've passed through in the HTML)
-    switch (operation) {
+    switch (s_operation) {
         //If what we've passed through in the function is "add" then we will use this calculation
         case "add":
-            result = number_one + number_two;
-            operator = $('#add').text();
+            result = i_number_one + i_number_two;
             break;
             //If what we've passed through in the function is "subtract" then we will use this calculation
         case "subtract":
-            result = number_one - number_two;
-            operator = $('#subtract').text();
+            result = i_number_one - i_number_two;
             break;
             //If what we've passed through in the function is "multiply" then we will use this calculation
         case "multiply":
-            result = number_one * number_two;
-            operator = $('#multiply').text();
+            result = i_number_one * i_number_two;
             break;
             //If what we've passed through in the function is "divide" then we will use this calculation
         case "divide":
-            result = number_one / number_two;
-            operator = String.fromCharCode(247);
+            result = i_number_one / i_number_two;
             break;
     }
 
-    outputResult(result, operator);
-    //     Call function which clears input boxes
-    clearNumbers();
-    //     return false to stop the page being submitted
-    return false;
+    $('#sum').val(result);
 };
-
-// // function which is called when the user clicks the add button - need to put window. due to using webpack compiler and ES6. (Only if we wanted to use in the HTML).
-// window.addNumbers = function() {
-//     //     Call the function which gets the two numbers from the input boxes
-//     if (!getNumbers()) {
-//         return false;
-//     }
-//     //     Store in a variable the two numbers added together
-//     var result = number_one + number_two;
-//     //Store the operator in a variable
-//     var operator = $('#add').text();
-//     //     Call the function which output the caculated number on the screen, providing the number to output
-//     // These two params do not need to be the same name they can simply be outputResult(number_one + number_two, $('add').text());
-//     outputResult(result, operator);
-//     //     Call function which clears input boxes
-//     clearNumbers();
-//     //     return false to stop the page being submitted
-//     return false;
-// };
-
-// // function which is called when the user clicks the subtract button- need to put window. due to using webpack compiler and ES6. (Only if we wanted to use in the HTML).
-// window.subtractNumbers = function() {
-//     //     Call the function which gets the two numbers from the input boxes
-//     if (!getNumbers()) {
-//         return false;
-//     }
-//     //     Store in a variable the two numbers subtracted
-//     var result = number_one - number_two;
-//     //Store the operator in a variable
-//     var operator = $('#subtract').text();
-//     //     Call the function which output the caculated number on the screen, providing the number to output
-//     outputResult(result, operator);
-//     //     Call function which clears input boxes
-//     clearNumbers();
-//     //     return false to stop the page being submitted
-//     return false;
-// };
-
-// // function which is called when the user clicks the multiply button- need to put window. due to using webpack compiler and ES6. (Only if we wanted to use in the HTML).
-// window.multiplyNumbers = function() {
-//     //     Call the function which gets the two numbers from the input boxes
-//     if (!getNumbers()) {
-//         return false;
-//     }
-//     //     Store in a variable the two numbers multiply together
-//     var result = number_one * number_two;
-//     //Store the operator in a variable
-//     var operator = $('#multiply').text();
-//     //     Call the function which output the caculated number on the screen, providing the number to output
-//     outputResult(result, operator);
-//     //     Call function which clears input boxes
-//     clearNumbers();
-//     //     return false to stop the page being submitted
-//     return false;
-// };
-
-// // function which is called when the user clicks the divide button- need to put window. due to using webpack compiler and ES6. (Only if we wanted to use in the HTML).
-// window.divideNumbers = function() {
-//     //     Call the function which gets the two numbers from the input boxes
-//     if (!getNumbers()) {
-//         return false;
-//     }
-//     //     Store in a variable the two numbers divided
-//     var result = number_one / number_two;
-//     //Store the operator in a variable
-//     var operator = String.fromCharCode(247);
-//     //     Call the function which output the caculated number on the screen, providing the number to output
-//     outputResult(result, operator);
-//     //     Call function which clears input boxes
-//     clearNumbers();
-//     //     return false to stop the page being submitted
-//     return false;
-// };
